5ac4774d18a1fc3d9e243f0b2e726c2e0e039938
JennyDigital <jennifer.a.gunn@outlook.com>
2024-04-17 21:27:38 +0100

    Added open source notice.

---
+++ main.c
@@ -0,0 +1,62 @@
+/** Callsign Beacon.
+  * ----------------
+  *
+  * Written esp for Steve Norman by Jennifer Gunn.
+  */
+
+
+/** Revision History:
+  * =================
+  *
+  *
+  * Version 1.0: 
+  * Initial release.
+  */
+#include <16F819.h>
+
+#device PIC16F819
+#device PASS_STRINGS = IN_RAM
+#FUSES INTRC_IO, NOWDT, PUT, MCLR, NOBROWNOUT, CCPB3
+#USE DELAY ( CLOCK = 2M )
+
+
+
+
+#define TICKRATE	142u
+
+#include <string.h>
+#include "hardware.h"
+#include "IRQ_Handler.h"
+#include "morse.h"
+
+extern volatile long long int systick;
+extern volatile long long int ident_period;
+
+int result;
+
+void SendIdent( char * msg )
+{
+
+}
+
+
+int main( void )
+{
+  ServiceInterruptInit();
+
+  LED_PWR_On( 1 );
+
+
+  while( 1 )
+  {
+    LED_Beat( 1 );
+
+    ident_period = 1000u * 60u * 10u;
+
+    Morse_puts("Code by Jennifer Gunn.");
+
+    LED_Beat( 0 );
+
+    while( ident_period );
+  }
+}
